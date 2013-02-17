70f9dae013071c37a9ba86ff18e9b4fa7ccfd8dd
ravithokala <t.ravisankar@gmail.com>
2013-02-17 00:24:30 +0000

    Added functionality to the latest default rate is updated on browser status in icon

---
+++ fxrates/eventPage.js
@@ -0,0 +1,78 @@
+function normalize(num) {
+    if (num < 10) {
+        return "0" + num;
+    } else {
+        return "" + num;
+    }
+}
+
+function getCurrentDate() {
+    var currTime = new Date();
+    var date = normalize(currTime.getDate()) + "/"
+        + normalize(currTime.getMonth() + 1) + "/"
+        + currTime.getFullYear();
+    var time = normalize(currTime.getHours()) + ":"
+        + normalize(currTime.getMinutes()) + ":"
+        + normalize(currTime.getSeconds());
+    return date + " " + time;
+}
+
+function updateIcon() {
+    chrome.browserAction.setBadgeText({
+        text: localStorage.defaultFxrate + ""
+    });
+}
+
+function updateDefaultFxRate(fxRates) {
+    localStorage.defaultFxrate = fxRates.gbp_inr;
+    updateIcon();
+}
+
+function getRate(fxPair, rates1HTML, numPlaces, decimalPlaces) {
+    var fxpair = new RegExp(fxPair + ": [0-9]{" + numPlaces + "}.[0-9]{" + decimalPlaces + "}", "g").exec(rates1HTML);
+    return new RegExp("[0-9]{" + numPlaces + "}.[0-9]{" + decimalPlaces + "}", "g").exec(fxpair);
+}
+
+function getLatestFromSBI(callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://www.sbiuk.com/", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            // WARNING! Might be injecting a malicious script!
+            var hiddenDiv = document.createElement("div");
+            document.body.appendChild(hiddenDiv);
+            hiddenDiv.style.display = "none";
+            hiddenDiv.innerHTML = /<body[^>]*>([\s\S]+)<\/body>/i.exec(xhr.responseText)[1];
+            var ratesHTML = hiddenDiv.getElementsByClassName("scroller")[0].childNodes[1].innerHTML;
+            var gbp_inr = getRate("GBP/INR", ratesHTML, 2, 2);
+            var usd_inr = getRate("USD/INR", ratesHTML, 2, 2);
+            var eur_inr = getRate("EUR/INR", ratesHTML, 2, 2);
+            var gbp_usd = getRate("GBP/USD", ratesHTML, 1, 4);
+            var fxRates = {
+                gbp_inr: gbp_inr,
+                usd_inr: usd_inr,
+                eur_inr: eur_inr,
+                gbp_usd: gbp_usd
+            }
+            callback(fxRates);
+        }
+    }
+    xhr.send(null);
+}
+
+if (chrome.runtime && chrome.runtime.onStartup) {
+    chrome.runtime.onStartup.addListener(function () {
+        console.log('Starting browser... updating icon.');
+        getLatestFromSBI(updateDefaultFxRate);
+    });
+} else {
+    // This hack is needed because Chrome 22 does not persist browserAction icon
+    // state, and also doesn't expose onStartup. So the icon always starts out in
+    // wrong state. We don't actually use onStartup except as a clue that we're
+    // in a version of Chrome that has this problem.
+    chrome.windows.onCreated.addListener(function () {
+        console.log('Window created... updating icon.');
+        getLatestFromSBI(updateDefaultFxRate);
+    });
+}
+
--- fxrates/popup.js
+++ fxrates/popup.js
@@ -1,61 +1,12 @@
-var rates = {
-    getLatestFromSBI: function () {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://www.sbiuk.com/", true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4) {
-                // WARNING! Might be injecting a malicious script!
-                var hiddenDiv = document.createElement("div");
-                document.body.appendChild(hiddenDiv);
-                hiddenDiv.style.display = "none";
-                hiddenDiv.innerHTML = /<body[^>]*>([\s\S]+)<\/body>/i.exec(xhr.responseText)[1];
-                var ratesHTML = hiddenDiv.getElementsByClassName("scroller")[0].childNodes[1].innerHTML;
-                var gbp_inr = rates.getRate("GBP/INR", ratesHTML, 2, 2)
-                var usd_inr = rates.getRate("USD/INR", ratesHTML, 2, 2);
-                var eur_inr = rates.getRate("EUR/INR", ratesHTML, 2, 2);
-                var gbp_usd = rates.getRate("GBP/USD", ratesHTML, 1, 4);
-                document.getElementById("gbp_inr").innerHTML = gbp_inr;
-                document.getElementById("usd_inr").innerHTML = usd_inr;
-                document.getElementById("eur_inr").innerHTML = eur_inr
-                document.getElementById("gbp_usd").innerHTML = gbp_usd;
-                document.getElementById("curr_time").innerHTML = rates.getCurrentDate();
+function updateDom(fxRates){
+    document.getElementById("gbp_inr").innerHTML = fxRates.gbp_inr;
+    document.getElementById("usd_inr").innerHTML = fxRates.usd_inr;
+    document.getElementById("eur_inr").innerHTML = fxRates.eur_inr
+    document.getElementById("gbp_usd").innerHTML = fxRates.gbp_usd;
+    document.getElementById("curr_time").innerHTML = getCurrentDate();
+    updateDefaultFxRate(fxRates);
+}
 
-                document.body.removeChild(hiddenDiv);
-                //chrome.browserAction.setBadgeText({text: '' + gbp_inr + ''});
-            }
-        }
-        xhr.send(null);
-    },
-
-    getRate: function (fxPair, ratesHTML, numPlaces, decimalPlaces) {
-        var fxpair = new RegExp(fxPair + ": [0-9]{" + numPlaces + "}.[0-9]{" + decimalPlaces + "}", "g").exec(ratesHTML);
-        return new RegExp("[0-9]{" + numPlaces + "}.[0-9]{" + decimalPlaces + "}", "g").exec(fxpair);
-    },
-
-    getCurrentDate: function () {
-        var currTime = new Date();
-        var date = rates.normalize(currTime.getDate()) + "/"
-            + rates.normalize(currTime.getMonth() + 1) + "/"
-            + currTime.getFullYear();
-        var time = rates.normalize(currTime.getHours()) + ":"
-            + rates.normalize(currTime.getMinutes()) + ":"
-            + rates.normalize(currTime.getSeconds());
-        return date + " " + time;
-    },
-
-    normalize: function (num) {
-        if (num < 10) {
-            return "0" + num;
-        } else {
-            return "" + num;
-        }
-    }
-
-
-};
-
-
-// Run our kitten generation script as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
-    rates.getLatestFromSBI();
+    getLatestFromSBI(updateDom);
 });
